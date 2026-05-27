afe02850f3ebed7cf4443c4f911e15966ca250a3
raffll <18517548+raffll@users.noreply.github.com>
2026-05-27 20:24:08 +0200

    Update .build.ps1

--- .build.ps1
+++ .build.ps1
@@ -4,4 +4,4 @@ if (Test-Path "$Name.zip") {
     Remove-Item "$Name.zip"
 }
 
-& "C:\Program Files\7-Zip\7z.exe" a -tzip "$Name.zip" "SPT Limits.omwscripts" "README.md" -r "scripts\" -r "l10n\"
+& "C:\Program Files\7-Zip\7z.exe" a -tzip "$Name.zip" "SPT Limits.omwscripts" "README.md" -r "scripts\" "l10n\"
