f9319f6df93a9468565522108c4f97338d0319b3
raffll <18517548+raffll@users.noreply.github.com>
2026-05-30 13:56:34 +0200

    wip

--- .build.ps1
+++ .build.ps1
@@ -1,10 +1,10 @@
 $Name = "SPT Limits"
 
-$tes3conv = "..\Remastered Rebalance Redux\tes3conv\tes3conv.exe"
-& $tes3conv "SPT.json" "SPT.esp" -o
+$tes3conv = "..\..\..\..\tes3conv\tes3conv.exe"
+& $tes3conv "$Name.json" "$Name.esp" -o
 
 if (Test-Path "$Name.zip") {
     Remove-Item "$Name.zip"
 }
 
-& "C:\Program Files\7-Zip\7z.exe" a -tzip "$Name.zip" "SPT Limits.omwscripts" "SPT.esp" "README.md" -r "scripts\" "l10n\"
+& "C:\Program Files\7-Zip\7z.exe" a -tzip "$Name.zip" "$Name.omwscripts" "$Name.esp" "README.md" -r "scripts\" "l10n\"
